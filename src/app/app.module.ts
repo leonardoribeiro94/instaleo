2627c1abd2624d69ed2cb01357464004f0fc0f9d
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-07-15 19:43:24 -0300

    prototyped the sendo of image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
-import { PhotosPage } from '../pages/photos/photos';
-import { TakePicturePage } from '../pages/take-picture/take-picture';
-
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { SignupPage } from "../pages/signup/signup";
+import { PhotosPage } from "../pages/photos/photos";
+import { TakePicturePage } from "../pages/take-picture/take-picture";
+import { SendPhotoPage } from "../pages/send-photo/send-photo";
 
 @NgModule({
   declarations: [
@@ -19,12 +19,10 @@ import { TakePicturePage } from '../pages/take-picture/take-picture';
     LoginPage,
     SignupPage,
     PhotosPage,
-    TakePicturePage
+    TakePicturePage,
+    SendPhotoPage
   ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
@@ -32,12 +30,13 @@ import { TakePicturePage } from '../pages/take-picture/take-picture';
     LoginPage,
     SignupPage,
     PhotosPage,
-    TakePicturePage
+    TakePicturePage,
+    SendPhotoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
