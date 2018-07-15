04b441a431dfb114511a46dc16dc0dd3da20defe
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-07-15 18:38:36 -0300

    created card of prototype image

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
-import { LoginPage } from "../login/login";
+import { PhotosPage } from "../photos/photos";
 
 @Component({
   selector: "page-home",
@@ -11,7 +11,7 @@ export class HomePage {
   public profileTab: any;
 
   constructor(public navCtrl: NavController) {
-    this.photosTab = LoginPage;
-    this.profileTab = LoginPage;
+    this.photosTab = PhotosPage;
+    this.profileTab = PhotosPage;
   }
 }
---
+++ src/pages/photos/photos.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+  selector: "page-photos",
+  templateUrl: "photos.html"
+})
+export class PhotosPage {
+  constructor(public navCtrl: NavController) {}
+}
