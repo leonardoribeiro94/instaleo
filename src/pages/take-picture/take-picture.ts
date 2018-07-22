a79721e4990824a0dc8850f4166784f9656aca4b
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-07-22 20:04:58 -0300

    taking photo

--- src/pages/take-picture/take-picture.ts
+++ src/pages/take-picture/take-picture.ts
@@ -13,12 +13,36 @@ export class TakePicturePage {
     private modalCtrl: ModalController
   ) {}
 
+  ionViewDidLoad() {
+    let video = <any>document.getElementById("video");
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      });
+    }
+  }
+
   dismiss = () => {
     this.viewCtrl.dismiss();
   };
 
   takePicture = () => {
-    let modal = this.modalCtrl.create(SendPhotoPage);
-    modal.present();
+    var video = <any>document.getElementById("video");
+    var canvas = <any>document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    context.drawImage(video, 0, 0, 320, 240);
+
+    video.classList.add("animated");
+    video.classList.add("flash");
+
+    setTimeout(() => {
+      let modal = this.modalCtrl.create(SendPhotoPage, {
+        photo: canvas.toDataURL()
+      });
+
+      modal.present();
+    }, 800);
   };
 }
