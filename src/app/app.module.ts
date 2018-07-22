17106dbb4833ddc24efbda8e5dcce16ab62ab0c8
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-07-22 18:52:13 -0300

    creating an account with fiirebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
@@ -12,6 +15,17 @@ import { PhotosPage } from "../pages/photos/photos";
 import { TakePicturePage } from "../pages/take-picture/take-picture";
 import { SendPhotoPage } from "../pages/send-photo/send-photo";
 import { ProfilePage } from "../pages/profile/profile";
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCLtpRoJItr5QUR7mMQTe9AH-MrIq0I-Uw",
+    authDomain: "instaleo-eded3.firebaseapp.com",
+    databaseURL: "https://instaleo-eded3.firebaseio.com",
+    projectId: "instaleo-eded3",
+    storageBucket: "instaleo-eded3.appspot.com",
+    messagingSenderId: "554343223729"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -24,7 +38,13 @@ import { ProfilePage } from "../pages/profile/profile";
     SendPhotoPage,
     ProfilePage
   ],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
